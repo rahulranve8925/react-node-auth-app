5a4440cd97c4822c8e98e36ebf2307233134d1d2
rahulranve8925 <rahulranve8925@gmail.com>
2026-04-03 22:00:08 +0530

    fix router for github pages

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login';
 import { Welcome } from './components/Welcome';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       {/* <Login/> */}
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
             <Route path="/" element={<Login/>}></Route>
             <Route path="/welcome" element={<Welcome/>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
