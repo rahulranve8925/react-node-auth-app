a7221e67d0d0c40b4659253c33542577b2bf7880
rahulranve8925 <rahulranve8925@gmail.com>
2026-04-03 20:33:02 +0530

    Initial project setup

---
+++ src/components/Login.jsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react'
+import "../Styles/Login.css"
+import { useNavigate } from 'react-router-dom';
+function Login() {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const res = await fetch(("http://localhost:5000/login"), {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                }),
+            })
+
+            const data = await res.json();
+            if (!res.ok) {
+                alert(data.message);
+                return;
+            }
+             localStorage.setItem("token", data.token);
+            alert(data.message)
+        } catch (error) {
+            console.log(error);
+            alert("Server Error");
+        }
+
+
+
+        setError("");
+        if (!email || !password) {
+            setError("All fields are required");
+            return;
+        }
+        // Password length
+        if (password.length < 6) {
+            setError("Password must be at least 6 characters");
+            return;
+        }
+        navigate("/welcome", { state: { email: email } })
+    }
+
+    return (
+        <div>
+            <div className="container">
+                <div className="login-box">
+                    <h2>Login</h2>
+                    <form onSubmit={handleSubmit}>
+                        <input type="email" placeholder="Enter Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <input type="password" placeholder="Enter Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        <button type="submit">Login</button>
+                        {error && <p className="error">{error}</p>}
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import "../Styles/Welcome.css"  
+
+export const Welcome = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+    // const email = location.state.email;
+    const [email,setEmail] = useState("");
+
+    useEffect(()=>{
+        const fetchUser = async ()=>{
+            const token = localStorage.getItem("token");
+              if (!token) {
+                navigate("/");
+                return;
+            }
+
+            try{
+                const res = await fetch("http://localhost:5000/protected",{
+                     method:"GET",
+                     headers:{
+                        Authorization: token,
+                     },
+                })
+
+                const data = await res.json();
+                console.log("dataaaaaa",data);
+                
+                setEmail(data.user.email)
+            }catch(err){
+                console.log(err);
+                
+            }
+        }
+        fetchUser();
+    },[])
+
+    const handleLogout = ()=>{
+         localStorage.removeItem("token");
+         navigate("/");
+    }
+    
+    return (
+        <div>
+            <div className="welcome-text">Welcome 🎉</div>
+                <p style={{ fontSize: "20px" }}>
+                    Hello, {email ? email : "User"} 👋
+                </p>
+            <button onClick={handleLogout}>Logout</button>
+        </div>
+    )
+}
